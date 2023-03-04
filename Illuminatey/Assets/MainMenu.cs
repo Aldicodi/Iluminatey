e9eb2ccfdee81aecd203762bd500b0c872b4c410
Aldicodi <backyardynamics@gmail.com>
2023-03-04 15:28:13 -0800

    Button functionality
    just what it says lmao

---
+++ Illuminatey/Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void StartInfo()
+    {
+        SceneManager.LoadScene("InfoScreen");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
