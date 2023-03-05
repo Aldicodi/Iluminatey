d83c3281ff7dc7dc45aeadb099414de7edd97b30
ktripuraneni <126908650+ktripuraneni@users.noreply.github.com>
2023-03-04 17:16:29 -0800

    EndScreenCode

--- Illuminatey/Assets/EndStats.cs
+++ Illuminatey/Assets/EndStats.cs
@@ -7,12 +7,30 @@ public class EndStats : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int moneySpent = 0;
+        int moneyMade = 0;
+        int tasksDone = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool nighttime = true;
+        bool taskComplete = false;
+        bool lightOn = false;
+        int bank = 0;
+
+        while (nighttime)
+        {
+           if (taskComplete)
+            {
+                bank += 1;
+            }
+           if (lightOn)
+            {
+                bank -= 1;
+            }
+        }
         
     }
 }
